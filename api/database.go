8be74862539374d97443c627fe60fcf9a8b80dcb
artcevvv <artcevvv@yandex.kz>
2025-05-24 18:10:54 +0500

    qwer

--- api/database.go
+++ api/database.go
@@ -1,58 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	_ = godotenv.Load("../.env")
-
-	// Get database configuration from environment variables
-	dbHost := os.Getenv("PGSQL_HOST")
-	dbUser := os.Getenv("PGSQL_USER")
-	dbPassword := os.Getenv("PGSQL_PSWRD")
-	dbPort := os.Getenv("PGSQL_PORT")
-	dbName := os.Getenv("PGSQL_DB")
-
-	// First connect to postgres database to create our database if needed
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable",
-		dbHost, dbUser, dbPassword, dbPort)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Open SQLite database (creates file if not exists)
+	db, err := gorm.Open(sqlite.Open("resumem.db"), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to PSQL:", err)
-	}
-
-	var count int64
-	db.Raw("SELECT COUNT(*) FROM pg_database WHERE datname = ?", dbName).Scan(&count)
-
-	if count == 0 {
-		createDBSQL := fmt.Sprintf("CREATE DATABASE %s;", dbName)
-		if err := db.Exec(createDBSQL).Error; err != nil {
-			log.Fatal("Failed to create db:", err)
-		}
-		log.Printf("Database %s created successfully", dbName)
-	}
-
-	sqlDB, err := db.DB()
-	if err != nil {
-		log.Fatal("Failed to get database instance: ", err)
-	}
-	sqlDB.Close()
-
-	// Now connect to our actual database
-	dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		dbHost, dbUser, dbPassword, dbName, dbPort)
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+		log.Fatal("Failed to connect to SQLite:", err)
 	}
 
 	err = db.AutoMigrate(&User{}, &Resume{}, &Link{}, &Skill{}, &Education{}, &Career{}, &Course{})
@@ -61,5 +22,5 @@ func InitDB() {
 	}
 
 	DB = db
-	log.Println("Database Connected successfully")
+	log.Println("SQLite Database Connected successfully")
 }
--- api/routes.go
+++ api/routes.go
@@ -10,14 +10,19 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
+	// Disable proxy trust checking since we're not behind a proxy
+	r.SetTrustedProxies(nil)
+
 	config := cors.DefaultConfig()
 	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
 	if allowedOrigins == "" {
-		allowedOrigins = "http://localhost:3000" // Default for development
+		allowedOrigins = "https://resumem.vercel.app/"
 	}
 	config.AllowOrigins = []string{allowedOrigins}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowCredentials = true
+	config.MaxAge = 12 * 60 * 60 // Cache preflight requests for 12 hours
 	r.Use(cors.New(config))
 
 	r.POST("/api/v1/register", Register)
